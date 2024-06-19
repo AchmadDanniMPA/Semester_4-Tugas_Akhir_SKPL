6d36d274e9417bdc11afa2fc2d2e7d06e1b92256
Danni <113399342+AchmadDanniMPA@users.noreply.github.com>
2024-06-19 13:56:41 +0700

    Update Query

--- Query DB PemWebTugasAkhir.sql
+++ Query DB PemWebTugasAkhir.sql
@@ -32,7 +32,7 @@ CREATE TABLE m_survey_soal (
     survey_id int,
     kategori_id int,
     no_urut int,
-    soal_jenis enum('Isian', 'Rating'),
+    soal_jenis enum('Rating','Isian'),
     soal_nama varchar(500),
     FOREIGN KEY (survey_id) REFERENCES m_survey(survey_id),
     FOREIGN KEY (kategori_id) REFERENCES m_kategori(kategori_id)
